bc80a0bc901e41e4d5b3c9ebc01346a41c8acdfd
DionysusBenstein <dionysusbenstein@gmail.com>
2022-03-14 10:44:55 +0300

    Create trade log database

---
+++ postgres/create_trade_log.sql
@@ -0,0 +1,41 @@
+-- CREATE DATABASE trade_log;
+
+CREATE TABLE slice_balance (
+    "id"            SERIAL NOT NULL PRIMARY KEY,
+    "user_id"       INT NOT NULL,
+    "asset"         VARCHAR(4) NOT NULL,
+    "type"          SMALLINT NOT NULL,
+    "balance"       DECIMAL(30,16) NOT NULL
+);
+
+CREATE TABLE slice_order (
+    "id"            SERIAL NOT NULL PRIMARY KEY,
+    "side"          SMALLINT NOT NULL,
+    "create_time"   TIMESTAMP NOT NULL,
+    "finish_time"   TIMESTAMP NOT NULL,
+    "user_id"       INT NOT NULL,
+    "market"        VARCHAR(7) NOT NULL,
+    "price"         DECIMAL(30,8) NOT NULL,
+    "amount"        DECIMAL(30,8) NOT NULL,
+    "taker_fee"     DECIMAL(30,4) NOT NULL,
+    "maker_fee"     DECIMAL(30,4) NOT NULL,
+    "left"          DECIMAL(30,8) NOT NULL,
+    "freeze"        DECIMAL(30,8) NOT NULL,
+    "deal_stock"    DECIMAL(30,8) NOT NULL,
+    "deal_money"    DECIMAL(30,16) NOT NULL,
+    "deal_fee"      DECIMAL(30,12) NOT NULL
+);
+
+CREATE TABLE slice_history (
+    "id"            SERIAL NOT NULL PRIMARY KEY,
+    "time"          TIMESTAMP NOT NULL,
+    "end_oper_id"   INT NOT NULL,
+    "end_order_id"  INT NOT NULL,
+    "end_deals_id"  INT NOT NULL
+);
+
+CREATE TABLE operlog (
+    "id"            SERIAL NOT NULL PRIMARY KEY,
+    "time"          TIMESTAMP NOT NULL,
+    "detail"        TEXT
+);
---
+++ postgres/init_trade_log.sql
@@ -0,0 +1,13 @@
+-- slice_balance
+INSERT INTO "slice_balance" ("id", "user_id", "asset", "type", "balance")
+VALUES 
+    (1, 1, 'BTC', 1, 10),
+    (2, 1, 'ETH', 2, 10),
+    (3, 1, 'XRP', 1, 10),
+    (4, 1, 'SOL', 2, 10),
+    (5, 1, 'DOGE', 2, 10),
+    (6, 1, 'USDT', 1, 10),
+    (7, 1, 'TRX', 2, 10),
+    (8, 1, 'LTC', 2, 10),
+    (9, 1, 'DASH', 2, 10),
+    (10, 1, 'BNB', 1, 10);
