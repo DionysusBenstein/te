ea84e9d4a23d62a67240062999346f33dcf4a7c4
Довран Джуманкулиев <ddowran2106@gmail.com>
2022-07-08 10:54:19 +0300

    remoed comment and fixed docker-compose

--- marketprice/src/kafka/kafka.consumer.ts
+++ marketprice/src/kafka/kafka.consumer.ts
@@ -29,12 +29,6 @@ class KafkaConsumer {
 
       await consumer.subscribe({ topic });
       await consumer.run({ eachMessage });
-      // const {CRASH} = consumer.events;
-
-      // await consumer.on(CRASH,  ()=>{
-      //   consumer.disconnect();
-      //   return this.subscribe(topic, eachMessage)
-      // })
 
       return consumer;
     } catch (err) {
