8e3293c8cccf128e5f8a5458318e4f9dedb849cd
Довран Джуманкулиев <ddowran2106@gmail.com>
2022-07-17 12:04:28 +0300

    change sql query with join

--- matchengine/src/database/transfer_data.ts
+++ matchengine/src/database/transfer_data.ts
@@ -53,7 +53,12 @@ async function openOrders() {
 
 async function deals() {
   const tradeMapping: any = await sequelize.query(
-    "SELECT * FROM TradeMapping WHERE TradeMapping.createdAt >= getdate()-1 ORDER BY CONVERT(datetime, TradeMapping.createdAt)", {
+    `
+    SELECT * FROM TradeMapping tm
+    LEFT JOIN TradeOrders AS tor ON tor.orderId = (CASE WHEN tm.type = 'buy' THEN tm.buyerOrderId ELSE tm.sellerOrderId )  
+    WHERE tm.createdAt >= getdate()-1 ORDER BY CONVERT(datetime, tm.createdAt)
+    `
+    , {
     type: QueryTypes.SELECT
   });
 
