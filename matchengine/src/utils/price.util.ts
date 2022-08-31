21ee48f1bdba5622413b2fe44973345dfbbcbce4
DionysusBenstein <dionysusbenstein@gmail.com>
2022-08-31 20:45:16 +0300

    Change GBEX usd price logic

--- matchengine/src/utils/price.util.ts
+++ matchengine/src/utils/price.util.ts
@@ -3,6 +3,19 @@ import { QueryTypes } from 'sequelize';
 
 export async function getAssetUsdPrice(assetName: string): Promise<number> {
   const MSSERVER_MISSING_CURRENCY = { usdPrice: 0 };
+
+  if (assetName === 'GBEX') {
+    const usdPriceQueryResult: any = await sequelize.query(
+      'SELECT price as usdPrice FROM ExchangeAssetPrice WHERE currencyName = :assetName AND deletedAt is NULL', {
+      replacements: { assetName },
+      plain: true,
+      raw: true,
+      type: QueryTypes.SELECT
+    });
+
+    return (usdPriceQueryResult || MSSERVER_MISSING_CURRENCY).usdPrice;
+  }
+
   const usdPriceQueryResult: any = await sequelize.query(
     'SELECT usdPrice from LivePrice WHERE currencyName = :assetName AND deletedAt is NULL', {
     replacements: { assetName },
